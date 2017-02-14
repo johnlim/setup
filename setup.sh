df1d7a1dfb7d93e53861f932dc93a40cad2eb77d
johnlim <johnlim.dingwei@gmail.com>
2017-02-14 14:25:54 +0800

    Add script to upgrade existing casks.

--- setup.sh
+++ setup.sh
@@ -6,13 +6,38 @@ RETSTATUS=$?
 if [ $RETSTATUS -ne 0 ]
 then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew update
 else
   tput setaf 1 && tput smul
   echo Warning:
   tput sgr0
   echo Homebrew already installed.
-fi
+  echo Upgrading brew casks.....
+  #http://apple.stackexchange.com/questions/190072/is-there-any-way-to-upgrade-brew-caski
+  # one-liner equivalent = curl -s https://gist.githubusercontent.com/atais/9c72e469b1cbec35c7c430ce03de2a6b/raw/36808a0544628398f26b48f7a3c7b309872ca2c6/cask_upgrade.sh | bash /dev/stdin 
+  (set -x; brew update;)
+
+  (set -x; brew cleanup;)
+  (set -x; brew cask cleanup;)
+
+  red=`tput setaf 1`
+  green=`tput setaf 2`
+  reset=`tput sgr0`
+
+  casks=( $(brew cask list) )
+
+  for cask in ${casks[@]}
+  do
+    version=$(brew cask info $cask | sed -n "s/$cask:\ \(.*\)/\1/p")
+    installed=$(find "/usr/local/Caskroom/$cask" -type d -maxdepth 1 -maxdepth 1 -name "$version") 
+    if [[ -z $installed ]]; then
+      echo "${red}${cask}${reset} requires ${red}update${reset}."
+      (set -x; brew cask uninstall $cask --force;)
+      (set -x; brew cask install $cask --force;)
+    else
+      echo "${red}${cask}${reset} is ${green}up-to-date${reset}."
+    fi
+  done
+  fi
 
 #Install Vim
 brew install vim
