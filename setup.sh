bc3ce7f9a4ba6a4f60044e06be3ef93f91c4fd75
huubap <huubap@huubaps-MacBook-Pro.local>
2018-11-16 17:57:45 +0800

    Updated setup file commands and removed progams which are no longer frequently used.

--- setup.sh
+++ setup.sh
@@ -1,64 +1,18 @@
 #!/bin/bash
-# Install dotfiles
-DOTFILES_REMOTE='https://github.com/johnlim/dotfiles.git' 
-DOTFILES_LOCAL=~/git/dotfiles
-git clone $DOTFILES_REMOTE $DOTFILES_LOCAL 
-chmod +x $DOTFILES_LOCAL/setup.sh
-$DOTFILES_LOCAL/setup.sh
-
-#install NVM (Node Version Manager)
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-source $HOME/.bash_profile
-nvm install node
-
-#install bower, grunt-cli, browserify and gulp 
-npm install -g bower
-npm install -g grunt-cli
-npm install -g browserify
-npm rm -g gulp
-npm i -g gulp-cli
-npm install -g polymer-cli
-npm install -g web-component-tester
-npm install -g wt-cli 
-
 # Install  Homebrew. Homebrew  is the equivalent of apt-get for linux. Its required to install tools such as git and wget.
 # Mac OS 10.9 will automatically prompt user to install XCode command line tools which is a pre-requisite.
 which brew 
 RETSTATUS=$?
 if [ $RETSTATUS -ne 0 ]
 then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
   tput setaf 1 && tput smul
   echo Warning:
   tput sgr0
   echo Homebrew already installed.
-  echo Upgrading brew casks.....
-  #http://apple.stackexchange.com/questions/190072/is-there-any-way-to-upgrade-brew-caski
-  # one-liner equivalent = curl -s https://gist.githubusercontent.com/atais/9c72e469b1cbec35c7c430ce03de2a6b/raw/36808a0544628398f26b48f7a3c7b309872ca2c6/cask_upgrade.sh | bash /dev/stdin 
-  (set -x; brew update;)
-
-  (set -x; brew cleanup;)
-  (set -x; brew cask cleanup;)
-
-  red=`tput setaf 1`
-  green=`tput setaf 2`
-  reset=`tput sgr0`
-
-  casks=( $(brew cask list) )
-
-  for cask in ${casks[@]}
-  do
-    version=$(brew cask info $cask | sed -n "s/$cask:\ \(.*\)/\1/p")
-    installed=$(find "/usr/local/Caskroom/$cask" -type d -maxdepth 1 -maxdepth 1 -name "$version") 
-    if [[ -z $installed ]]; then
-      echo "${red}${cask}${reset} requires ${red}update${reset}."
-      (set -x; brew cask uninstall $cask --force;)
-      (set -x; brew cask install $cask --force;)
-    else
-      echo "${red}${cask}${reset} is ${green}up-to-date${reset}."
-    fi
-  done
+  echo Updating formulae and homebrew itself.....
+  (set -x; brew update;) 
 fi
 
 #Homebrew will show warning message if already installed.  
@@ -73,7 +27,7 @@ brew list | grep kdiff3
 RETSTATUS=$?
 if [ $RETSTATUS -ne 0 ]
 then
-  brew install Caskroom/cask/kdiff3
+  brew cask install kdiff3
   git config --global diff.tool kdiff3
   git config --global merge.tool kdiff3
   git config --global core.editor "/usr/local/bin/vim"
@@ -85,15 +39,13 @@ else
 fi
 
 # Install SDKMAN
-curl -s get.sdkman.io | bash
+curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 sdk install java
 
 # Install G* tools
 sdk install groovy
-sdk install grails
-sdk install gradle
 
 # Install wget
 brew install wget
@@ -101,35 +53,11 @@ brew install wget
 # Install python (comes with Setuptools and pip)
 brew install python 
 
-#install automake
-brew install automake
-
-#install libtool
-brew install libtool 
-
-#install ruby
-brew install ruby
-gem install jekyll
-gem install compass
-
-# Install Homebrew-cask. A CLI workflow for the adminisration of Mac applications distributed as binaries http://caskroom.io
-echo Tapping caskroom/cask...
-brew tap caskroom/cask
-brew install brew-cask 
-
 # Install IntelliJ Ulitmate
 brew cask install intellij-idea
 
 # Install Alfred
-brew cask list | grep alfred 
-RETSTATUS=$?
-if [ $RETSTATUS -ne 0 ]
-then
-  brew cask install alfred 
-  brew cask alfred 
-else
-  brew cask install alfred #use brew casks' warning message
-fi
+brew cask install alfred 
 
 # Install SequelPro
 brew cask install sequel-pro
@@ -139,4 +67,16 @@ brew cask install skype
 
 # Install Chrome
 brew cask install google-chrome
+
+# Install dotfiles
+DOTFILES_REMOTE='https://github.com/johnlim/dotfiles.git' 
+DOTFILES_LOCAL=~/git/dotfiles
+git clone $DOTFILES_REMOTE $DOTFILES_LOCAL 
+chmod +x $DOTFILES_LOCAL/setup.sh
+$DOTFILES_LOCAL/setup.sh
+
+#install NVM (Node Version Manager) - the script adds the source line to your prfile (~/bash_profile etc) so pls install dotfiles first.
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+source $HOME/.bash_profile
+nvm install node
 
